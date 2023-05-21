e5953eb70cb1baa2ea7ade168f189c230017eded
nayoon <nayoonk928@gmail.com>
2023-05-22 01:11:31 +0900

    리팩토링

--- build/resources/main/data.sql
+++
@@ -1,6 +0,0 @@
-insert into account_user(id, name, created_at, updated_at)
-values (1, 'Pororo', now(), now());
-insert into account_user(id, name, created_at, updated_at)
-values (2, 'Lupi', now(), now());
-insert into account_user(id, name, created_at, updated_at)
-values (3, 'Eddie', now(), now());
